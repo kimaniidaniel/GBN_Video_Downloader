ea665565dd39fcea5e6308bb233fe94f681358fe
Kimanii <kimaniidaniel@gmail.com>
2015-02-19 10:21:07 -0400

    first commit

---
+++ GBN_Video_Downloader/GBN_Video_Downloader.py
@@ -0,0 +1,15 @@
+import re
+import requests
+
+try:
+    data = requests.get("https://www.facebook.com/gbntelevisionnews")
+#except  as e:
+#    raise MyException("There was an error: %r" % e)
+
+page = data.text
+links = re.findall("(https:\/\/www.facebook.com\/photo.php\?v\=(\d+))",page.read())
+if links.count > 1:
+    distinctLinks = list(set(links))
+    for x in range(0,len(distinctLinks)):
+        print (distinctLinks[x])
+    print("Total:",len(distinctLinks))
