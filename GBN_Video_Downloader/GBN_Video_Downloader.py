5a197321dc28f1fcbe8bea23ada9877969334beb
kimaniidaniel <kimaniidaniel@kimaniis-imac>
2015-03-01 12:59:52 -0400

    Logging and better download via urlretreive

--- GBN_Video_Downloader/GBN_Video_Downloader.py
+++ GBN_Video_Downloader/GBN_Video_Downloader.py
@@ -1,15 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import re
-#import requests
+import datetime
+import time
 import os
 import io
 import sys
-import urllib.request
+print("GBN NEWS DOWNLOADER\nVersion 1.0")
+print("Python Version: %s\n" % sys.version)
 
-LOCALSTORE = "/temp/videos/GBNNews/"
-MAXPAGE = 5
-STARTPAGE = 3
+try:
+    import urllib.request as urlRequest
+except:
+    import urllib as urlRequest
+
+
+LOCALSTORE = "./GBNNews/"
+MAXPAGE = 15
+STARTPAGE = 0
 ARTICLES_PER_PAGE = 5
 BASEURL = "http://gbn.gd/en/gbnnews/1/?ls-art0="
+PAGE_REGEX = "<a href=\"(/en/gbnnews/\d+/\d+\/.+\.htm)\">"
+VIDEO_REGEX = "<a href=\"(/attachment/\d+/.+\.mp4)"
 
 #<a href="/en/gbnnews/1/927/$2m-for-Home-Grown-Programme.htm">$2m for Home-Grown Programme</a>
 class Downloader:
@@ -17,49 +30,62 @@ class Downloader:
     This downloads a page on the gbn.gd site
     """
     def getNewsFromPage(self, url):
-        #data = requests.get(url)
         data = self.getWebPage(url)
-        links = re.findall("<a href=\"(/en/gbnnews/\d+/\d+\/.+\.htm)\">", data)
+        links = re.findall(PAGE_REGEX, data)                                                #Find all video links on page
         if len(links) > 1:
             distinctLinks = list(set(links))
             for x in range(0, len(distinctLinks)):
                 print ("Processing: %s" % distinctLinks[x])
-                data = self.getWebPage("http://gbn.gd" + distinctLinks[x])
-                links = re.findall("<a href=\"(/attachment/\d+/.+\.mp4)", data)
+                try:
+                    data = self.getWebPage("http://gbn.gd" + distinctLinks[x])
+                except Exception as e:
+                    self.logError(e)
+                    continue
+                links = re.findall(VIDEO_REGEX, data)
                 distinctLinks2 = list(set(links))
                 for x in range(0, len(distinctLinks2)):
-                    vid = str.replace(distinctLinks2[x],'”','').replace('“','')
+                    vid = distinctLinks2[x]
                     print ("\t- Downloading Video: %s" % vid)
                     filename = re.findall("/attachment/\d+/(.+.mp4)", vid)
                     if not self.fileExists(LOCALSTORE + filename[0]):
                         if not os.path.exists(LOCALSTORE):
                             try:
                                 d = os.makedirs(LOCALSTORE)
-                                print (d)
-                                file = open(LOCALSTORE + filename[0], 'wb')
-                                response = getWebPage("http://gbn.gd" + vid, stream=True)
-                                for block in response.iter_content(1024):
-                                    if not block:
-                                        break
-                                    file.write(block)
-                                file.close()
                             except Exception as e:
-                                print("Unexpected error: ",e.args[0])
+                                self.logError(e)
+                        urlRequest.urlretrieve("http://gbn.gd" + vid,LOCALSTORE + filename[0])
+                        print("\t- Saved: %s" % LOCALSTORE + filename[0])
                     else:
                           print("\t- File already exists") 
             print("Total: %d articles discovered\n" % len(distinctLinks))
     
+    def logError(e):
+        errorText = ""
+        for x in e.args:
+            errorText += " " + str(x)
+        print("Unexpected error: ", errorText)
+        
     def fileExists(self,file):
         return os.path.isfile(file)
+
+    def progress_callback(blocks, block_size, total_size, d):
+        #blocks->data downloaded so far (first argument of your callback)
+        #block_size -> size of each block
+        #total-size -> size of the file
+        #implement code to calculate the percentage downloaded e.g
+        print "downloaded %f%%" % blocks/float(total_size)
     
     def getWebPage(self,url):
-        page = urllib.request.urlopen(url)
+        return urlRequest.urlopen(url).read()
         f = io.TextIOWrapper(page,encoding='utf-8')
-        return f.read()
+        return page.read()
 
 d = Downloader()
-for x in range(STARTPAGE,MAXPAGE*ARTICLES_PER_PAGE,ARTICLES_PER_PAGE):
+MAXPAGE +=1
+ts = time.time()
+print "Started: " + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+for x in range(STARTPAGE*ARTICLES_PER_PAGE,MAXPAGE*ARTICLES_PER_PAGE,ARTICLES_PER_PAGE):
     print("Page %d" % (x / ARTICLES_PER_PAGE))
     d.getNewsFromPage(BASEURL+str(x))
-
-print("ALL DONE!")
+ts = time.time()
+print "ENDED: " + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
