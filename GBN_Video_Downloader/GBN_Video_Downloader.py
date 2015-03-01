3da7e4989dd342027275ed3c11835062b6e12e75
kimaniidaniel <kimaniidaniel@kimaniis-imac>
2015-03-01 09:18:37 -0400

    Improved structure

--- GBN_Video_Downloader/GBN_Video_Downloader.py
+++ GBN_Video_Downloader/GBN_Video_Downloader.py
@@ -1,15 +1,50 @@
 import re
 import requests
+import os
 
-try:
-    data = requests.get("https://www.facebook.com/gbntelevisionnews")
-#except  as e:
-#    raise MyException("There was an error: %r" % e)
+LOCALSTORE = "/Users/kimaniidaniel/Desktop/GBNNews/"
+MAXPAGE = 5
+STARTPAGE = 3
+ARTICLES_PER_PAGE = 5
+BASEURL = "http://gbn.gd/en/gbnnews/1/?ls-art0="
 
-page = data.text
-links = re.findall("(https:\/\/www.facebook.com\/photo.php\?v\=(\d+))",data.text)
-if links.count > 1:
-    distinctLinks = list(set(links))
-    for x in range(0,len(distinctLinks)):
-        print (distinctLinks[x])
-    print("Total:",len(distinctLinks))
+#<a href="/en/gbnnews/1/927/$2m-for-Home-Grown-Programme.htm">$2m for Home-Grown Programme</a>
+class Downloader:
+    """
+    This downloads a page on the gbn.gd site
+    """
+    def getNewsFromPage(self, url):
+        data = requests.get(url)
+        links = re.findall("<a href=\"(/en/gbnnews/\d+/\d+\/.+\.htm)\">", data.text)
+        if len(links) > 1:
+            distinctLinks = list(set(links))
+            for x in range(0, len(distinctLinks)):
+                print ("Processing: %s" % distinctLinks[x])
+                data = requests.get("http://gbn.gd" + distinctLinks[x])
+                links = re.findall("<a href=\"(/attachment/\d+/.+\.mp4)", data.text)
+                distinctLinks2 = list(set(links))
+                for x in range(0, len(distinctLinks2)):
+                    print (distinctLinks2[x])
+                    print ("\t- Downloading Video: %s" % distinctLinks2[x])
+                    filename = re.findall("/attachment/\d+/(.+.mp4)", distinctLinks2[x])
+                    if self.fileExists(LOCALSTORE + filename[0]) == False:
+                        file = open(LOCALSTORE + filename[0], 'wb')
+                        response = requests.get("http://gbn.gd" + distinctLinks2[x], stream=True)
+                        for block in response.iter_content(1024):
+                            if not block:
+                                break
+                            file.write(block)
+                        file.close()
+                    else:
+                          print("\t- File already exists") 
+            print("Total: %d articles discovered\n" % len(distinctLinks))
+    
+    def fileExists(self,file):
+        return os.path.isfile(file)
+
+d = Downloader()
+for x in range(STARTPAGE,MAXPAGE*ARTICLES_PER_PAGE,ARTICLES_PER_PAGE):
+    print("Page %d" % (x / ARTICLES_PER_PAGE))
+    d.getNewsFromPage(BASEURL+str(x))
+
+print("ALL DONE!")
