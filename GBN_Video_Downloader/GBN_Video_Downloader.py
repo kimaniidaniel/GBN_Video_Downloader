e3ff858373f2c3e3aecf0fa9d555bcc9f01deca5
Kimanii <kimaniidaniel@gmail.com>
2015-03-02 11:17:21 -0400

    Added documentation of the code

--- GBN_Video_Downloader/GBN_Video_Downloader.py
+++ GBN_Video_Downloader/GBN_Video_Downloader.py
@@ -7,6 +7,7 @@ import time
 import os
 import io
 import sys
+
 print("GBN NEWS DOWNLOADER\nVersion 1.0")
 print("Python Version: %s\n" % sys.version)
 
@@ -15,51 +16,63 @@ try:
 except:
     import urllib as urlRequest
 
+#----------------------SETTINGS----------------------------
+# Edit these settings as required
+LOCALSTORE = "GBNNews/"                                     # directory to store downloaded videos
+MAXPAGE = 15                                                # max number of pages to scan for videos
+STARTPAGE = 0                                               # page to start scanning from
+ARTICLES_PER_PAGE = 5                                       # expected articles on each page
+BASEURL = "http://gbn.gd/en/gbnnews/1/?ls-art0="            # base URL to look for pages
+PAGE_REGEX = "<a href=\"(/en/gbnnews/\d+/\d+\/.+\.htm)\">"  # regex used to find the respective pages for the articles eg <a href="/en/gbnnews/1/927/$2m-for-Home-Grown-Programme.htm">$2m for Home-Grown Programme</a>
+VIDEO_REGEX = "<a href=\"(/attachment/\d+/.+\.mp4)"         # regex used to find the videos on the article pages
+GBNSITE = "http://gbn.gd"                                   # GBN main site URL
+#-------- END: Edit nothing under this line ---------------
 
-LOCALSTORE = "./GBNNews/"
-MAXPAGE = 15
-STARTPAGE = 0
-ARTICLES_PER_PAGE = 5
-BASEURL = "http://gbn.gd/en/gbnnews/1/?ls-art0="
-PAGE_REGEX = "<a href=\"(/en/gbnnews/\d+/\d+\/.+\.htm)\">"
-VIDEO_REGEX = "<a href=\"(/attachment/\d+/.+\.mp4)"
 
-#<a href="/en/gbnnews/1/927/$2m-for-Home-Grown-Programme.htm">$2m for Home-Grown Programme</a>
+
+
 class Downloader:
     """
     This downloads a page on the gbn.gd site
     """
     def getNewsFromPage(self, url):
         data = self.getWebPage(url)
-        links = re.findall(PAGE_REGEX, data)                                                #Find all video links on page
+        links = re.findall(PAGE_REGEX, data)                                                # Find all article links on page
+        
         if len(links) > 1:
             distinctLinks = list(set(links))
-            for x in range(0, len(distinctLinks)):
+            for x in range(0, len(distinctLinks)):                                          # Iterate through distinct links
                 print ("Processing: %s" % distinctLinks[x])
                 try:
-                    data = self.getWebPage("http://gbn.gd" + distinctLinks[x])
+                    data = self.getWebPage(GBNSITE + distinctLinks[x])
                 except Exception as e:
                     self.logError(e)
                     continue
-                links = re.findall(VIDEO_REGEX, data)
+
+                links = re.findall(VIDEO_REGEX, data)                                       # Find the video link on the article page
                 distinctLinks2 = list(set(links))
-                for x in range(0, len(distinctLinks2)):
+
+                for x in range(0, len(distinctLinks2)):                                     # Download each video on that page that's found above 
                     vid = distinctLinks2[x]
                     print ("\t- Downloading Video: %s" % vid)
-                    filename = re.findall("/attachment/\d+/(.+.mp4)", vid)
+                    filename = re.findall("/attachment/\d+/(.+.mp4)", vid)                  # Extract the file name from the video link
                     if not self.fileExists(LOCALSTORE + filename[0]):
-                        if not os.path.exists(LOCALSTORE):
+                        if not os.path.exists(LOCALSTORE):                                  # Try to create the local directory if it's missing
                             try:
                                 d = os.makedirs(LOCALSTORE)
                             except Exception as e:
                                 self.logError(e)
-                        urlRequest.urlretrieve("http://gbn.gd" + vid,LOCALSTORE + filename[0])
+                        
+                        urlRequest.urlretrieve(GBNSITE + vid,LOCALSTORE + filename[0])      # Actual downloading of the video
                         print("\t- Saved: %s" % LOCALSTORE + filename[0])
                     else:
                           print("\t- File already exists") 
             print("Total: %d articles discovered\n" % len(distinctLinks))
     
     def logError(e):
+        """
+        Used to log all errors and display to screen
+        """
         errorText = ""
         for x in e.args:
             errorText += " " + str(x)
@@ -76,14 +89,15 @@ class Downloader:
         print "downloaded %f%%" % blocks/float(total_size)
     
     def getWebPage(self,url):
+        """
+        Download a web page and return the bytes
+        """
         return urlRequest.urlopen(url).read()
-        f = io.TextIOWrapper(page,encoding='utf-8')
-        return page.read()
 
 d = Downloader()
 MAXPAGE +=1
-ts = time.time()
-print "Started: " + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+print "Started: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 for x in range(STARTPAGE*ARTICLES_PER_PAGE,MAXPAGE*ARTICLES_PER_PAGE,ARTICLES_PER_PAGE):
     print("Page %d" % (x / ARTICLES_PER_PAGE))
     d.getNewsFromPage(BASEURL+str(x))
